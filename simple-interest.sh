b6197537048a5a22b71eb06feb3e09544ce5aac3
omvijay220-ship-it <omvijay220@gmail.com>
2026-02-28 18:08:46 +0530

    simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 echo "Enter Principal:"
-read p
+read principal
 
 echo "Enter Rate of Interest:"
-read r
+read rate
 
 echo "Enter Time Period:"
-read t
+read time
 
-si=$((p*r*t/100))
+simple_interest=$(echo "scale=2; ($principal * $rate * $time) / 100" | bc)
 
-echo "Simple Interest is: $si"
+echo "Simple Interest is: $simple_interest"
